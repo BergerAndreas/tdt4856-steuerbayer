0aa15eb71182cf0a6bd117fd210a2e769d52d5db
Andreas Berger <andreas.berger.ab@gmail.com>
2019-02-06 13:31:05 +0100

    Fix python version for mac

--- eye_detection/main.py
+++ eye_detection/main.py
@@ -1,6 +1,10 @@
 import os
-
+import platform
 
 if __name__ == "__main__":
-    os.system("python detect_blinks.py "
-              "--shape-predictor shape_predictor_68_face_landmarks.dat")
+    if(platform.system() == 'Windows'):
+        os.system("python detect_blinks.py "
+            "--shape-predictor shape_predictor_68_face_landmarks.dat")
+    else:
+        os.system("python3 detect_blinks.py "
+            "--shape-predictor shape_predictor_68_face_landmarks.dat")
