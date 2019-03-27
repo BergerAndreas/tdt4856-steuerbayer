20572440f3d813e87db3bfcd0f30816bea1219bb
henrikbstrand <henrik.strand95@hotmail.com>
2019-03-27 10:46:25 +0100

    Oppdatert arduino

--- arduino/EiT_varsling/EiT_varsling.ino
+++ arduino/EiT_varsling/EiT_varsling.ino
@@ -1,16 +1,11 @@
 #define VIBRATOR  A5
 #define SPEAKER   3
 
-
 #define LED_GREEN 8
 #define LED1 9
 #define LED2 10
 #define LED3 11
 #define LED4 12
-
-
-
-int BassTab[] = {1911, 1702, 1516, 1431, 1275, 1136, 1012}; //bass 1~7
 
 
 void setup()
